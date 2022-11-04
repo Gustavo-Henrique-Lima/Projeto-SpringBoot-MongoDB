49b3741dce52df21f1c802dcff976c6e0968facf
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-04 16:14:10 -0300

    Obtendo usuário por id

--- src/main/java/com/gustavohenrique/servicos/ServicosUsuario.java
+++ src/main/java/com/gustavohenrique/servicos/ServicosUsuario.java
@@ -1,11 +1,13 @@
 package com.gustavohenrique.servicos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavohenrique.dominio.Usuario;
+import com.gustavohenrique.exception.ObjectNotFoundException;
 import com.gustavohenrique.repositorios.RepositorioUsuario;
 
 @Service
@@ -17,5 +19,11 @@ public class ServicosUsuario {
 	public List<Usuario> findall()
 	{
 		return userRepo.findAll();
+	}
+	
+	public Usuario findById(String id) 
+	{
+		Optional<Usuario> obj = userRepo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
